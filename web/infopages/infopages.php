f0cb4a86d7b775513bc018ea407a8bdb1d39e117
A. Kaan Atesoglu <aka43@case.edu>
2016-03-05 17:20:32 -0500

    trying a different connection

--- web/infopages/infopages.php
+++ web/infopages/infopages.php
@@ -5,7 +5,19 @@
 <body>
 <?php
   echo "reached the informational pages";
-  include sql_connection.php;
+  
+  $servername = "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$username = "blwusy6p9r16bm03";
+$password = "z0j9g1st5u43bf3g";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
 </body>
 </html>
