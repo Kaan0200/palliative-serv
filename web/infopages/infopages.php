fb20b50577f46e736c1819635a2ec0abdfd06657
A. Kaan Atesoglu <aka43@case.edu>
2016-03-05 17:27:43 -0500

    moved sql php connection file

--- web/infopages/infopages.php
+++ web/infopages/infopages.php
@@ -1,23 +1,11 @@
 <html>
 <head>
-	<?php include '../views/header.html'; ?>
+	<?php 
+	include '../views/header.html';
+	include '../sql_connection.php';
+	?>
 </head>
 <body>
-<?php
-  echo "reached the informational pages";
-  
-  $servername = "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
-$username = "blwusy6p9r16bm03";
-$password = "z0j9g1st5u43bf3g";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-?>
+  sql_connect();
 </body>
 </html>
--- web/sql_connection.php
+++ web/sql_connection.php
@@ -3,12 +3,15 @@ $servername = "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
 $username = "blwusy6p9r16bm03";
 $password = "z0j9g1st5u43bf3g";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+function sql_connect() {
 
-// Check connection
-if ($conn->connect_error) {
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
+
+  // Check connection
+  if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+  } 
+  echo "Connected successfully";
+}
 ?>
