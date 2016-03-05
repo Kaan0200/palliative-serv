7d18b98e770f4ee614e7af1f110a8916b362a706
A. Kaan Atesoglu <aka43@case.edu>
2016-03-05 17:53:09 -0500

    reworked php sql connect

--- web/sql_connection.php
+++ web/sql_connection.php
@@ -1,9 +1,10 @@
 <?php
+
+function sql_connect() {
+
 $servername = "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
 $username = "blwusy6p9r16bm03";
 $password = "z0j9g1st5u43bf3g";
-
-function sql_connect() {
 
   // Create connection
   $conn = new mysqli($servername, $username, $password);
