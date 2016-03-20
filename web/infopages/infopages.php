a0ae5390ec48721d0b615203031a18c77c71dce4
A. Kaan Atesoglu <aka43@case.edu>
2016-03-20 18:21:14 -0400

    Adding the basics of a form
    Signed-off-by: A. Kaan Atesoglu <aka43@case.edu>

--- web/infopages/infopages.php
+++ web/infopages/infopages.php
@@ -7,7 +7,32 @@
 </head>
 <body>
 <?php
-  sql_connect();
+
+  $page_id = -1;
+  // empty variable holders
+  $title = $subtitle = $text = $subtext = $linktext = "";
+  $parent_id = 0;
+  
+  
+  
 ?>
+
+<dir>
+ Parent Page: <select> </select>
+ Title: <input type="text"> </input>
+ Subtitle: <input type="text"> </input>
+ 
+ Text: <input type="text"></input>
+ Detail Text: <input type="text"></input>
+ 
+ Link Text: <input type="text"></input>
+ 
+</dir>
+
+
+
 </body>
+<?php
+  include '../views/footer.html';
+?>
 </html>
