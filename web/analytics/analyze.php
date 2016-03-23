1db3875d3f5a96d93c9d55f4595964638c7717d7
A. Kaan Atesoglu <aka43@case.edu>
2016-03-23 15:17:11 -0400

    added sql call to analytics page
    Signed-off-by: A. Kaan Atesoglu <aka43@case.edu>

--- web/analytics/analyze.php
+++ web/analytics/analyze.php
@@ -1,10 +1,13 @@
 <html>
 <head>
 	<?php include '../views/header.html'; ?>
+	<?php include '../sql_connection.php'; ?>
 </head>
 <body>
 <?php
   echo "reached the analytics page";
+  
+  echo sql_connection();
 ?>
 </body>
 </html>
