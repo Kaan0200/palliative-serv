3091fc07be7050c6d55577a4298e0aebc318646e
A. Kaan Atesoglu <aka43@case.edu>
2016-03-23 16:05:48 -0400

    added table forming from sql call
    Signed-off-by: A. Kaan Atesoglu <aka43@case.edu>

--- web/analytics/analyze.php
+++ web/analytics/analyze.php
@@ -6,7 +6,18 @@
 <body>
 <?php 
   sql_connect();
-  select_star("test");
+  $result->select_star("test");
+  
+  echo '<table cellpadding="0" cellspacing="0" class="db-table">';
+  echo '<tr><th>ID</th><th>A</th><th>B</th>';
+  while ($row = mysqli_fetch_row($result)) {
+	  echo '<tr>';
+	  foreach ($row as $key => $value) {
+		  echo '<td>',$value,'</td>'; 
+	  }
+	  echo '</tr>';
+  }
+  echo '</table><br/>';
 ?>
 </body>
 </html>
