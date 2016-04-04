5372e1495be7451aca1d162498b1983743764826
Kaan0200 <aka43@case.edu>
2016-04-04 13:21:20 -0400

    Added very simple form to quizzing system
    Signed-off-by: Kaan0200 <aka43@case.edu>

--- web/quizzing/quizzes.php
+++ web/quizzing/quizzes.php
@@ -3,8 +3,27 @@
 	<?php include '../views/header.html'; ?>
 </head>
 <body>
+<h2>Use this page to edit, add, or delete pages that already exist in the application.</h2>
+<hr>
+
+<form action="" method="post">
+  <div>
+    <div class="LabelColumn">Test Input:</div>
+	<div class="InputColumn">
+		<input type="text" name="testinput" />
+	</div> 
+  </div>
+  <input name="submit" type="submit">
+</form>
+
+<hr>
+
 <?php
-  echo "reached the quizzing system page";
-?>
+	if (isset($_POST['submit'])) {
+		$test = $_POST['testinput'];
+		echo $test . ": this stuff";
+	}
+
 </body>
+<?php include '../views/footer.html'; ?>
 </html>
