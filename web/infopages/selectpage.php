b609f3f638f874a6880b273bbfb51becc35c890e
Kaan0200 <aka43@case.edu>
2016-04-07 01:38:37 -0400

    Added infopage select
    Signed-off-by: Kaan0200 <aka43@case.edu>

---
+++ web/infopages/selectpage.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<?php 
+	include '../views/header.html';
+	?>
+</head>
+<body>
+<h2>Select an existing page to edit or create a new info page.</h2>
+<hr>
+
+<div>
+	<div>
+		<div>
+			Select Page:
+		</div>
+		<div>
+			<select name="ParentSelect"> </select>">
+		</div>
+	</div>
+</div>
+
+<style>
+  .LabelColumn {
+	float: left;
+	width: 20%;
+	padding-top: 2px;
+  }
+  .InputColumn {
+	padding-top: 2px;
+  }
+</style>
+
+</body>
+<?php
+  include '../views/footer.html';
+?>
+</html>
