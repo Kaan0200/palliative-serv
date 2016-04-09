4aaec265c3b21b5c1bb79f2d35fb3a72e9ad7613
A. Kaan Atesoglu <aka43@case.edu>
2016-04-09 19:36:41 -0400

    added layout for login page
    Signed-off-by: A. Kaan Atesoglu <aka43@case.edu>

--- web/index.php
+++ web/index.php
@@ -3,7 +3,17 @@
   <?php include 'views/header.html'; ?>
 </head>
 <body>
-
+  <form>
+	<div>
+		<div>Username:</div>
+		<input type="text">
+	</div>
+	<div>
+		<div>Password:</div>
+		<input type="text">
+	</div>
+	<input type="submit" name="submit" value="Submit">
+  </form>
 </body>
 <footer>
 </footer>
