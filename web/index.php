bedc60e5ef210afde9f3cb0defef25a9e6525072
A. Kaan Atesoglu <aka43@case.edu>
2016-04-09 19:15:59 -0400

    Added new page for logging in
    Signed-off-by: A. Kaan Atesoglu <aka43@case.edu>

--- web/index.php
+++ web/index.php
@@ -3,48 +3,7 @@
   <?php include 'views/header.html'; ?>
 </head>
 <body>
-	<div class="mainColumn">
-		<div class="leftColumn">
-			<p>Use this to access the informational page system. Here you can modify pages, add or remove pages, and change
-			the hierarchical structure.</p>
-		</div>
-		<div class="rightColumn">
-			<a href="infopages/selectpage.php">
-				<div class="squircleButton_right">Informational Heirachy</div>
-			</a>
-		</div>
-	</div>
-	<div class="mainColumn">
-		<div class="leftColumn"> 
-			<a href="quizzing/quizzes.php">
-				<div class="squircleButton_left">Quizzing System</div>
-			</a>
-		</div>
-		<div class="rightColumn">
-			<p> Access the quizzing system to remove modify the applications quizzes and questions.</p>
-		</div>
-	</div>
-	<div class="mainColumn">
-		<div class="leftColumn"> 
-			<p> View the database tables relating to application usage and what type of users use the application. </p>
-		</div>
-		<div class="rightColumn">
-			<a href="analytics/analyze.php">
-				<div class="squircleButton_right">Application Analytics</div>
-			</a>
-		</div>
-	</div>
-	<div class="mainColumn">
-		<div class="leftColumn"> 
-			<a href="settings/settings.php">
-				<div class="squircleButton_left">Admin Settings</div>
-			</a>
-		</div>
-		<div class="rightColumn">
-			<p> Modify settings for the online portal, including login information and user name info. </p>
-		</div>
-	</div>
+
 </body>
 <footer>
-  <?php include 'views/footer.html'; ?>
 </footer>
