f9f7dfafc6bed1d3b139eee6e9ffb202564b3daa
A. Kaan Atesoglu <aka43@case.edu>
2016-04-09 20:53:02 -0400

    Added beginnings of login scripting
    Signed-off-by: A. Kaan Atesoglu <aka43@case.edu>

--- web/index.php
+++ web/index.php
@@ -3,14 +3,28 @@
   <?php include 'views/header.html'; ?>
 </head>
 <body>
-  <form class="form">
+<?php
+  include("sql_connection.php");
+  
+  $connection = get_connection();
+
+ session_start();
+ 
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$username = mysqli_real_escape_string($connection, $_POST['username']);
+	$password = mysqli_real_escape_string($connection, $_POST['password']);
+	
+	$sql = "SELECT id FROM settings WHERE username = '$username' and password = '$password'";
+ }
+?>
+  <form class="form" method="post">
 	<div>
 		<div>Username:</div>
-		<input type="text">
+		<input type="text" name="username">
 	</div>
 	<div>
 		<div>Password:</div>
-		<input type="text">
+		<input type="password" name="password">
 	</div>
 	<input type="submit" name="submit" value="Login">
   </form>
