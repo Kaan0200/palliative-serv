364df6afc8b30d9e2a7f44b616c4e98b61a26c70
A. Kaan Atesoglu <aka43@case.edu>
2016-04-11 15:08:07 -0400

    swapped index and logout names because bad
    Signed-off-by: A. Kaan Atesoglu <aka43@case.edu>

--- web/logout.php
+++ web/logout.php
@@ -3,54 +3,10 @@
   <?php include 'views/header.html'; ?>
 </head>
 <body>
-<?php
-  include("sql_connection.php");
-  
-  $connection = get_connection();
-  
-  session_start();
- 
- if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	 
-	 // start session now
-	 session_start();
-
-	$username = mysqli_real_escape_string($connection, $_POST['username']);
-	$password = mysqli_real_escape_string($connection, $_POST['password']);
+	<?php
+	session_destroy();
+	echo "You are now logged out.  You will be redirected to the login in 10 seconds...";
 	
-	$sql = "SELECT id FROM settings WHERE name = 'username' and value = '".$username."' UNION SELECT id from settings WHERE name = 'password' and value = '".$password."'";
-	
-	$result = mysqli_query($connection, $sql);
-	$count = mysqli_num_rows($result);
-	
-	if ($count == 2) {
-		$_SESSION['login_user']=$username;
-		header("location: menu.php");
-	} else {
-		$error = "Username or Password is invalid";
-	}
-	mysqli_close($connection);
- }
-?>
-  <form class="form" method="post">
-	<div>
-		<div>Username:</div>
-		<input type="text" name="username">
-	</div>
-	<div>
-		<div>Password:</div>
-		<input type="password" name="password">
-	</div>
-	<input type="submit" name="submit" value="Login">
-  </form>
-  <?php echo $error ?>
+	?>
+	<meta http-equiv="refresh" content="10;url=http://palliative-serv.herokuapp.com/index.php">
 </body>
-<style>
- .form{
-	text-align: center;
-    padding: 10px;
- }
- input {
-	 margin: 5px;
- }
-</style>
