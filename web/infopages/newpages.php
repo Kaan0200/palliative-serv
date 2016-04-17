3ebb841dbcdbadbac7e03594cbcb1cd42d44289c
Kaan0200 <aka43@case.edu>
2016-04-17 04:02:02 -0400

    added success message
    Signed-off-by: Kaan0200 <aka43@case.edu>

--- web/infopages/newpages.php
+++ web/infopages/newpages.php
@@ -25,8 +25,53 @@
 	}
 	// get value
 	$parent_id = $_GET['id'];
+
+if (isset($_POST['Submit'])) {
+	// handle the title
+	if (empty($_POST["title"])) {
+		$titleErr = "Title is required for an article";
+	} else {
+		$title = clean_input($_POST["title"]);
+		if (!preg_match("/^[a-zA-Z0-9 \?\!]*$/",$title)) {
+			$titleErr = "Only letters, numbers, punctuation, and white space allowed in the title";
+		}
+	}
+	// handle the maintext
+	$text = clean_input($_POST["text"]);
+	if (preg_match("[^\w\.@-]", $text)) {
+		$textErr = "Invalid characters included in text block.  Allowed characters are \n";
+		$textErr = $textErr . "periods, @ symbol, underscore, or hyphen.";
+	}
+	// handle the detail text
+	$detailtext = clean_input($_POST["detailtext"]);
+	if (preg_match("[^\w\.@-]", $detailtext)) {
+		$textDetErr = "Invalid characters included in detail text block.  Allowed characters\n";
+		$textDetErr = $textDetErr . "are periods, @ symbol, underscore, or hyphen.";
+	}
+	// handle the link text
+	$linktext = clean_input($_POST["linktext"]);
+	if (!preg_match("/^[a-zA-Z0-9 \?\!]*$/",$linktext)) {
+		$linkTextErr = "Only letters, numbers, punctuation, and white space allowed in the subtitle";
+	}
 	
-  
+	
+	error_log("-----------------submitted form--------------------");
+	if ($titleErr == "" and $textErr == "" and $textDetErr == "" and $linktextErr == "") {
+		// handle form submission
+		$con = get_connection();
+		$sql = "CALL Insert_Page(".$parent_id.",\"".$title."\", \"".$text."\", \"".$detailtext."\", \"".$linktext."\");";
+		error_log("-----".$sql."-----");
+		mysqli_query($con, $sql);
+		mysqli_close($con);
+		echo "<h3 style=\"color:green\">Page successfully submitted!</h3>"
+	} else {
+		echo $parenterr;
+		echo $titleErr;
+		echo $textErr;
+		echo $textDetErr;
+		echo $linktextErr;
+	}
+}
 ?>
 <h2>Use this page to add new pages to the application.</h2>
 <hr>
@@ -70,51 +115,6 @@
 	</div>
 </div>
 <input type="submit" name="Submit" value="Submit">
-<?php
-	if (isset($_POST['Submit'])) {
-	// handle the title
-	if (empty($_POST["title"])) {
-		$titleErr = "Title is required for an article";
-	} else {
-		$title = clean_input($_POST["title"]);
-		if (!preg_match("/^[a-zA-Z0-9 \?\!]*$/",$title)) {
-			$titleErr = "Only letters, numbers, punctuation, and white space allowed in the title";
-		}
-	}
-	// handle the maintext
-	$text = clean_input($_POST["text"]);
-	if (preg_match("[^\w\.@-]", $text)) {
-		$textErr = "Invalid characters included in text block.  Allowed characters are \n";
-		$textErr = $textErr . "periods, @ symbol, underscore, or hyphen.";
-	}
-	// handle the detail text
-	$detailtext = clean_input($_POST["detailtext"]);
-	if (preg_match("[^\w\.@-]", $detailtext)) {
-		$textDetErr = "Invalid characters included in detail text block.  Allowed characters\n";
-		$textDetErr = $textDetErr . "are periods, @ symbol, underscore, or hyphen.";
-	}
-	// handle the link text
-	$linktext = clean_input($_POST["linktext"]);
-	if (!preg_match("/^[a-zA-Z0-9 \?\!]*$/",$linktext)) {
-		$linkTextErr = "Only letters, numbers, punctuation, and white space allowed in the subtitle";
-	}
-	error_log("-----------------submitted form--------------------");
-	if ($titleErr == "" and $textErr == "" and $textDetErr == "" and $linktextErr == "") {
-		// handle form submission
-		$con = get_connection();
-		$sql = "CALL Insert_Page(".$parent_id.",\"".$title."\", \"".$text."\", \"".$detailtext."\", \"".$linktext."\");";
-		error_log("-----".$sql."-----");
-		mysqli_query($con, $sql);
-		mysqli_close($con);
-	} else {
-		echo $parenterr;
-		echo $titleErr;
-		echo $textErr;
-		echo $textDetErr;
-		echo $linktextErr;
-	}
-	}
-?>
 </form>
 </body>
 <footer>
