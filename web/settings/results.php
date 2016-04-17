f28b2cb02e872264b33b36df27429d9c1260b086
Kaan0200 <aka43@case.edu>
2016-04-17 12:35:20 -0400

    functionality for settings page:
    Signed-off-by: Kaan0200 <aka43@case.edu>

---
+++ web/settings/results.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+	<?php 
+	include '../views/header.html';
+	include '../sql_connection.php';
+	
+	session_start();
+	if($_SESSION['valid'] == false){ header("Location: ../index.php"); }
+?>
+</head>
+<body>
+	<?php
+		$errText  = "";
+ 		$new_name = $_POST['username'];
+		$pass1    = $_POST['password1'];
+		$pass2    = $_POST['password2'];
+		
+		// check that username is alphanumeric
+		if (empty($new_name)) {
+			$errText = "Username cannot be empty. \n";
+		}
+		if (!preg_match("[a-zA-Z0-9]", $new_name)){
+			$errText = $errText."Username must be alphanumeric. \n";
+		}
+		//check that we are doing password1
+		if (!empty($pass1)){
+			if (!preg_match("[a-zA-Z0-9]",$pass1)){
+				$errText = $errText."Password must be alphanumeric. \n";
+			}
+			if (!strcmp($pass1, $pass2)){
+				$errText = $errText."Passwords are not the same. \n";
+			}
+		}
+		
+		// completely clean so submit
+		if ($errText == ""){
+			$con = get_connection();
+			error_log("submitted new username");
+			mysqli_query($con, "UPDATE settings SET value =\"".$new_name."\" WHERE id = 0");
+			echo "<h3 style=\"color:green\">Changed Username</h3>";
+			if (!empty($pass1)) {
+				error_log("submitted new password");
+				mysqli_query($con, "UPDATE settings SET value =\"".$pass1."\" WHERE id = 1");
+				echo "<h3 style=\"color:green\">Changed Password</h3>";
+			}
+		} else {
+			echo "<h3 style=\"color:red\">".$errText."</h3>";
+		}
+	?>
+
+</body>
+<?php
+	include '../views/footer.html';
+?>
+</html>
--- web/settings/settings.php
+++ web/settings/settings.php
@@ -1,10 +1,35 @@
 <html>
 <head>
-	<?php include '../views/header.html'; ?>
+	<?php 
+	include '../views/header.html';
+	include '../sql_connection.php';
+	
+	session_start();
+	if($_SESSION['valid'] == false){ header("Location: ../index.php"); }
+?>
 </head>
 <body>
-<?php
-  echo "reached the admin information page";
-?>
+	<h2>Use this page to modify login settings for this website.</h2>
+	<hr>
+	<form method="post" action="results.php">
+		<div>Username (alphanumeric)</div>
+		<input type="text" name="username" value="<?php 
+			$con = get_connection();
+			$result = mysqli_query($con, "SELECT value FROM settings WHERE id = 0");
+			if ($result->num_rows > 0){
+				while($row = mysqli_fetch_assoc($result)){
+					echo $row['value'];
+				}
+			}
+		?>"/>
+		<div>Password (Leave blank if you don't want to change)</div>
+		<input type="password" name="password1"></input>
+		<div>Repeat Password</div>
+		<input type="password" name="password2"></input>
+		<input type="submit" name="Submit" value="Save">
+	</form>
 </body>
+<?php
+	include '../views/footer.html';
+?>
 </html>
