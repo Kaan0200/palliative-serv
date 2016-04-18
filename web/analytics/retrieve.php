a8b470f239d8e2ba355d14a64a6d6e026671d35c
Kaan0200 <aka43@case.edu>
2016-04-18 17:12:58 -0400

    added php page for analytics retrieving
    Signed-off-by: Kaan0200 <aka43@case.edu>

---
+++ web/analytics/retrieve.php
@@ -0,0 +1,19 @@
+<?php
+  // use this for ingesting json that will be submitted into the server analytics
+  /*  EXAMPLE JSON
+  
+[ “credentials” : [
+    “device” : deviceID string
+    “age” : number as string
+    “postGrad” : bool as string
+    “years” : number as string
+    “certification” : specialty as string
+    “practice” : practice setting as string
+  ]
+ “page_hits” : [
+    page_id (int) : # of hits (int)
+ ]
+]
+  */
+
+?>
