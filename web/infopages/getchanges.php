5e5a8c8884e9b19853458080e8afd5446844e6e8
Kaan0200 <aka43@case.edu>
2016-04-19 18:46:08 -0400

    added json encoding
    Signed-off-by: Kaan0200 <aka43@case.edu>

--- web/infopages/getchanges.php
+++ web/infopages/getchanges.php
@@ -5,6 +5,17 @@
 	$mobile_version = $_GET['v'];
 
 	$con   = get_connection();
-	$query = "SELECT id, type, statement FROM updates WHERE id >= ".$mobile_version; 
- 
+	$query = "SELECT id, type, statement FROM updates WHERE id >= ".$mobile_version;
+	$result = mysqli_query($con, $query);
+	
+	// create empty array
+	$updates = array();
+	while ($row = mysqli_fetch_assoc($result)) {
+		$updates[] = $row
+	}
+	
+	echo json_encode($updates);
+	
+	mysqli_close($con);
+	
 ?>
