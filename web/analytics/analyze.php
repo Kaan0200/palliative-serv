5519587c2d997973454207845ea98aa5ed8db6e5
Kaan0200 <aka43@case.edu>
2016-04-20 12:12:30 -0400

    modified sql statement
    Signed-off-by: Kaan0200 <aka43@case.edu>

--- web/analytics/analyze.php
+++ web/analytics/analyze.php
@@ -17,18 +17,21 @@
 		echo "<h3>This table shows which pages in the application users most often view.</h3>";
 		echo "<table border=\"1\">";
 			echo "<tr><td>Page Title</td><td>Views</td></tr>";
-			$result = mysqli_query($con, "SELECT pages.title, SUM(count) AS c FROM stats JOIN pages WHERE page_id = pages.id GROUP BY page_id;");
+			$result = mysqli_query($con, "SELECT pages.title, SUM(count) AS c FROM stats JOIN pages WHERE page_id = pages.id GROUP BY page_id ORDER BY c DESC;");
 			if ($result->num_rows > 0) {
 				while($row = mysqli_fetch_assoc($result)){
 					echo "<tr><td>".substr($row['title'],0, 40)."</td><td>".$row['c']."</td></tr>";
 				}
 			}
 		echo "</table>";
-	} else if ($_GET['view'] == 2) {
-		echo "selected view 2";
-	} else if ($_GET['view'] == 3) {
+	}
+	else if ($_GET['view'] == 2) {
+		echo "<h3>These tables show various collected demographic information regarding who uses the mobile application.</h3>";
+	}
+	else if ($_GET['view'] == 3) {
 		echo "selected view 3";
-	} else {
+	}
+	else {
 	  	echo "<h3>Select a type of analysis</h3>";
 	}
 	mysqli_close($con);
