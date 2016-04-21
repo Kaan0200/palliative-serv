15eb02fd01211225f271eec8b451f71bab063acf
Kaan0200 <aka43@case.edu>
2016-04-20 20:33:40 -0400

    added proper column names
    Signed-off-by: Kaan0200 <aka43@case.edu>

--- web/analytics/analyze.php
+++ web/analytics/analyze.php
@@ -32,12 +32,12 @@
 		echo "<h3>These tables show various collected demographic information regarding who uses the mobile application.</h3>";
 		
 		// execute all age queries
-		$age1 = mysqli_query($con, "SELECT COUNT(id) FROM app_users WHERE age < 25;");
-		$age2 = mysqli_query($con, "SELECT COUNT(id) FROM app_users WHERE age >= 25 AND age < 30;");
-		$age3 = mysqli_query($con, "SELECT COUNT(id) FROM app_users WHERE age >= 30 AND age < 35;");
-		$age4 = mysqli_query($con, "SELECT COUNT(id) FROM app_users WHERE age >= 35 AND age < 40;");
-		$age5 = mysqli_query($con, "SELECT COUNT(id) FROM app_users WHERE age >= 40 AND age < 50;");
-		$age6 = mysqli_query($con, "SELECT COUNT(id) FROM app_users WHERE age >= 50;");
+		$age1 = mysqli_query($con, "SELECT COUNT(id) AS a FROM app_users WHERE age < 25;");
+		$age2 = mysqli_query($con, "SELECT COUNT(id) AS a FROM app_users WHERE age >= 25 AND age < 30;");
+		$age3 = mysqli_query($con, "SELECT COUNT(id) AS a FROM app_users WHERE age >= 30 AND age < 35;");
+		$age4 = mysqli_query($con, "SELECT COUNT(id) AS a FROM app_users WHERE age >= 35 AND age < 40;");
+		$age5 = mysqli_query($con, "SELECT COUNT(id) AS a FROM app_users WHERE age >= 40 AND age < 50;");
+		$age6 = mysqli_query($con, "SELECT COUNT(id) AS a FROM app_users WHERE age >= 50;");
 		// get the result
 		$result1 = mysqli_fetch_assoc($age1);
 		$result2 = mysqli_fetch_assoc($age2);
@@ -47,12 +47,12 @@
 		$result6 = mysqli_fetch_assoc($age6);
 		// make into a table
 		echo "<table border=\"1\"><tr><td>Age Bracket</td><td>User Count</td></tr>";
-		echo "<tr><td>Below 25</td><td>".$result1."</td></tr>";
-		echo "<tr><td>25 to 30</td><td>".$result2."</td></tr>";
-		echo "<tr><td>30 to 35</td><td>".$result3."</td></tr>";
-		echo "<tr><td>35 to 40</td><td>".$result4."</td></tr>";
-		echo "<tr><td>40 to 50</td><td>".$result5."</td></tr>";
-		echo "<tr><td>Above 50</td><td>".$result6."</td></tr>";
+		echo "<tr><td>Below 25</td><td>".$result1['a']."</td></tr>";
+		echo "<tr><td>25 to 30</td><td>".$result2['a']."</td></tr>";
+		echo "<tr><td>30 to 35</td><td>".$result3['a']."</td></tr>";
+		echo "<tr><td>35 to 40</td><td>".$result4['a']."</td></tr>";
+		echo "<tr><td>40 to 50</td><td>".$result5['a']."</td></tr>";
+		echo "<tr><td>Above 50</td><td>".$result6['a']."</td></tr>";
 		echo "</table>";
 	}
 	else {
