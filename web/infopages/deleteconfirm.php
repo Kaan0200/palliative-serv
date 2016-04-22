26d386c1ebd0db5b358783e17f11fe67b39e2daa
Kaan0200 <aka43@case.edu>
2016-04-22 17:14:15 -0400

    added code for delete confirm
    Signed-off-by: Kaan0200 <aka43@case.edu>

--- web/infopages/deleteconfirm.php
+++ web/infopages/deleteconfirm.php
@@ -11,8 +11,24 @@
 <body>
 <?php
   echo "<h3>Confirm deletion of existing page?</h3>";
-  echo "*This page will be completely unrecoverable, there is no undo."
+  echo "*This page will be completely unrecoverable, there is no undo.";
+  echo "<hr>";
+  
+  $page_to_del = $_GET["id"];
+  $con = get_connection();
+  $query = "SELECT COUNT(*) AS c FROM pages WHERE parent_id = ".$page_to_del.";";
+  $result = mysqli_query($con, $query);
+  while ($row = mysqli_fetch_assoc($result)) {
+	  echo "<h4>There are ".$row['c']." pages that are the children of the page selected to be deleted</h4>";
+  }
+  
+  if (isset($_POST['Submit'])) {
+	  
+  }
 ?>
+<form>
+	<input type="submit" name="Submit" value="Confirm Delete"></input>
+</form>
 </body>
 <footer>
 <?php
